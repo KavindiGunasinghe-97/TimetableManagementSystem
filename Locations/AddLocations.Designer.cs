fa2bc4aad1cadcda2613235cec58f2496b558a5b
KavindiGunasinghe <42095172+KavindiGunasinghe-97@users.noreply.github.com>
2020-10-13 13:44:45 +0530

    Add files via upload

---
+++ Locations/AddLocations.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Time_Table_Management_System.Locations
+{
+    partial class AddLocations
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxRoomName = new System.Windows.Forms.TextBox();
+            this.radioButtonLectureHall = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButtonLaboratory = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxCapacity = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.errorLocation = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.comboBoxBuildingName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorLocation)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.Info;
+            this.button1.Location = new System.Drawing.Point(3, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 25);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // title
+            // 
+            this.title.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.SteelBlue;
+            this.title.Location = new System.Drawing.Point(116, 30);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(209, 32);
+            this.title.TabIndex = 16;
+            this.title.Text = "Add Locations";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(29, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 20);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Add Rooms Building wise:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Info;
+            this.label2.Location = new System.Drawing.Point(29, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 20);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "Building Name";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Info;
+            this.label4.Location = new System.Drawing.Point(29, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 20);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "Room Name";
+            // 
+            // textBoxRoomName
+            // 
+            this.textBoxRoomName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxRoomName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.textBoxRoomName.Location = new System.Drawing.Point(257, 197);
+            this.textBoxRoomName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxRoomName.Name = "textBoxRoomName";
+            this.textBoxRoomName.Size = new System.Drawing.Size(248, 28);
+            this.textBoxRoomName.TabIndex = 35;
+            // 
+            // radioButtonLectureHall
+            // 
+            this.radioButtonLectureHall.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.radioButtonLectureHall.AutoSize = true;
+            this.radioButtonLectureHall.ForeColor = System.Drawing.SystemColors.Info;
+            this.radioButtonLectureHall.Location = new System.Drawing.Point(257, 272);
+            this.radioButtonLectureHall.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButtonLectureHall.Name = "radioButtonLectureHall";
+            this.radioButtonLectureHall.Size = new System.Drawing.Size(105, 21);
+            this.radioButtonLectureHall.TabIndex = 36;
+            this.radioButtonLectureHall.TabStop = true;
+            this.radioButtonLectureHall.Text = "Lecture Hall";
+            this.radioButtonLectureHall.UseVisualStyleBackColor = true;
+            this.radioButtonLectureHall.CheckedChanged += new System.EventHandler(this.radioButtonSem1_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Info;
+            this.label5.Location = new System.Drawing.Point(29, 271);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 20);
+            this.label5.TabIndex = 37;
+            this.label5.Text = "Room Type";
+            // 
+            // radioButtonLaboratory
+            // 
+            this.radioButtonLaboratory.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.radioButtonLaboratory.AutoSize = true;
+            this.radioButtonLaboratory.ForeColor = System.Drawing.SystemColors.Info;
+            this.radioButtonLaboratory.Location = new System.Drawing.Point(407, 272);
+            this.radioButtonLaboratory.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButtonLaboratory.Name = "radioButtonLaboratory";
+            this.radioButtonLaboratory.Size = new System.Drawing.Size(98, 21);
+            this.radioButtonLaboratory.TabIndex = 38;
+            this.radioButtonLaboratory.TabStop = true;
+            this.radioButtonLaboratory.Text = "Laboratory";
+            this.radioButtonLaboratory.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Info;
+            this.label6.Location = new System.Drawing.Point(29, 336);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 20);
+            this.label6.TabIndex = 39;
+            this.label6.Text = "Capacity";
+            // 
+            // textBoxCapacity
+            // 
+            this.textBoxCapacity.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxCapacity.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.textBoxCapacity.Location = new System.Drawing.Point(257, 333);
+            this.textBoxCapacity.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxCapacity.Name = "textBoxCapacity";
+            this.textBoxCapacity.Size = new System.Drawing.Size(248, 28);
+            this.textBoxCapacity.TabIndex = 40;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnSave.Location = new System.Drawing.Point(709, 378);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(120, 39);
+            this.btnSave.TabIndex = 41;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.BackColor = System.Drawing.Color.DarkRed;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnClear.Location = new System.Drawing.Point(560, 378);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(120, 39);
+            this.btnClear.TabIndex = 42;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // errorLocation
+            // 
+            this.errorLocation.ContainerControl = this;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // comboBoxBuildingName
+            // 
+            this.comboBoxBuildingName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBoxBuildingName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.comboBoxBuildingName.Location = new System.Drawing.Point(257, 136);
+            this.comboBoxBuildingName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBoxBuildingName.Name = "comboBoxBuildingName";
+            this.comboBoxBuildingName.Size = new System.Drawing.Size(248, 28);
+            this.comboBoxBuildingName.TabIndex = 43;
+            this.comboBoxBuildingName.TextChanged += new System.EventHandler(this.comboBoxBuilduing_TextChanged);
+            // 
+            // AddLocations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(881, 442);
+            this.Controls.Add(this.comboBoxBuildingName);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.textBoxCapacity);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.radioButtonLaboratory);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.radioButtonLectureHall);
+            this.Controls.Add(this.textBoxRoomName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "AddLocations";
+            this.Text = "AddLocations";
+            ((System.ComponentModel.ISupportInitialize)(this.errorLocation)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxRoomName;
+        private System.Windows.Forms.RadioButton radioButtonLectureHall;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButtonLaboratory;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxCapacity;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ErrorProvider errorLocation;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.TextBox comboBoxBuildingName;
+    }
+}
